2bcfd1ee54cffb0dd353b846978dba7279ed236e
KarVarr <megatr9n@gmail.com>
2022-05-11 18:37:17 +0300

    var_2

---
+++ app.js
@@ -0,0 +1,28 @@
+// firstDice
+
+let randomNumb = Math.floor(Math.random () * 6) +1;
+
+let randomImg = "dice" + randomNumb + ".svg"
+
+
+document.querySelector('img').setAttribute("src", randomImg);
+
+// secondDice
+
+let randomNumb2 = Math.floor(Math.random () * 6) +1;
+
+let randomImg2 = "dice" + randomNumb2 + ".svg"
+
+
+document.querySelector('.imgClass').setAttribute("src", randomImg2);
+
+
+// ifelse for flag
+
+if (randomNumb > randomNumb2 ) {
+    document.querySelector('i').classList.add('faFlag1');
+} else if (randomNumb < randomNumb2){
+    document.querySelector('.i2').classList.add('faFlag1');
+}
+
+
